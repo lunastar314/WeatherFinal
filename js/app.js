ad0be812c9a68a29b590617802c9bd2677028d93
Dielka Cueto <lunastar1118@gmail.com>
2014-12-11 23:49:18 -0500

    giving up for tonight.
    Got weather to show but all three on one page

--- js/app.js
+++ js/app.js
@@ -1,137 +1,132 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-	var apiKey = '9dd895f2e791fb8329d7d7f729d1e016';
-	var apiURL = 'https://api.forecast.io/forecast/' + apiKey;
-	var defaultLat = '40.8264';
-	var defaultLng = '-73.8786';
-	
-	// Request the user's Latitude/Longitude
-	if (Modernizr.geolocation) {
-				navigator.geolocation.getCurrentPosition(success, error);
-		}
-		else {
-			//Prompt User
-		}
-		
-		//Received a Latitude/Longitude from the browser
-		function success(position) {
-			console.log(position);
-			getWeatherWithPos(position.coords.latitude,position.coords.longitude);
-		}
-		
-		//Unable to find a Latitude/Longitude
-		function error(error) {
-			console.log(error);
-			getWeatherWithPos(defaultLat, defaultLng);
-		}
-		
-		//Request weather from forecast.io with a Latitude/Longitude
-		function getWeatherWithPos(lat,lng) 
-		{
-			//Construct the url to request
-			apiURL += "/" + lat + ',' +  lng;
-			console.log(apiURL);
-			
-			//Make a request to forecast.io
-			$.ajax({
-					url: apiURL,
-					type: "GET",
-					crossDomain: true,
-					dataType: 'jsonp',
-					success: function (response) {
-					//The request succeeded
-					console.log(response);
-					parseWeather(response);
-					$('#loader').remove();
-					},
-					error: function (xhr, status) {
-						console.log(status);
-						$('#loader').remove();
-						$('#error').removeClass("hidden");
-						showError();
-					}
-			});
-		}
-	
+    var apiKey = '9dd895f2e791fb8329d7d7f729d1e016';
+    var apiURL = 'https://api.forecast.io/forecast/' + apiKey;
+    var defaultLat = '40.8264';
+    var defaultLng = '-73.8786';
 
-	// Parse and use the weather values from the forecast.io JSON
-	
+    // Request the user's Latitude/Longitude
+    if (Modernizr.geolocation) {
+        navigator.geolocation.getCurrentPosition(success, error);
+    } else {
+        //Prompt User
+    }
 
-	function parseWeather(data) 
-	{
-			var week = data.daily.data;
+    //Received a Latitude/Longitude from the browser
+    function success(position) {
+        console.log(position);
+        getWeatherWithPos(position.coords.latitude, position.coords.longitude);
+    }
 
-		$('#temp').text("Now " +(Math.round(data.currently.apparentTemperature)));
-		$('#temp').addClass('degrees');
-		$('#sum').text(data.currently.summary
-		$('#temp2').text(Math.round(week[1].temperatureMin));
-		$('#temp2').addClass('degrees');
-		$('#sum2').text(week[1].summary);
-		$('#temp3').text(Math.round(week[2].temperatureMin));
-		$('#temp3').addClass('degrees');
-		$('#sum3').text(week[2].summary);	
+    //Unable to find a Latitude/Longitude
+    function error(error) {
+        console.log(error);
+        getWeatherWithPos(defaultLat, defaultLng);
+    }
 
-		console.log(week);
-			var today = data.currently.apparentTemperature;
-			var tomorrow = data.daily.data[1];
-			var dayAfterTomorrow = data.daily.data[2];	
-			console.log(tomorrow.temperatureMin);
-		
-		console.log(today);
-			var imageFile= parseIcon(data.currently.icon);
-			console.log("condition: "+ data.currently.icon);
-			console.log("image file: " + imageFile);
+    //Request weather from forecast.io with a Latitude/Longitude
+    function getWeatherWithPos(lat, lng) {
+        //Construct the url to request
+        apiURL += "/" + lat + ',' + lng;
+        console.log(apiURL);
 
-		 $('<img>').attr("src", "images/"+ imageFile).appendTo('#wrapper');
-		console.log(today.icon);
-		
-		var image = parseIcon(week[1].icon);
-			$('<img>').attr("src","images/" + image).appendTo('#wrapper');
-		
-		var image = parseIcon(week[2].icon);
-			$('<img>').attr("src","images/" + image).appendTo('#wrapper');
-	
-	 function parseIcon(icon)
-	 	{
-	    	switch(icon) 
-	    	{
-    			
-    			case "wind":
-    			case "partly-cloudy-day":	
-                	   var img = "partly-cloudy-day.jpg";
-			break;
-				
-				case "cloudy";
-				case "partly-cloudy-night":	
-                	   var img = "partly-cloudy-night.jpg";
-			break;
-
-				case "rain":
-                	  var img = "rainyDay.jpg";
-                	  var img = "rainyNight.jpg";
-			break;
-
-				case "snow":
-    				var img= "snowyDay.jpg"
-    			case "sleet":
-                	var img = "snowyNight.jpg";
-               
-            break;
-
-			break;
-    			default: "#d86b93";
-    				break;	
-			}
-		return img;
-    	}
+        //Make a request to forecast.io
+        $.ajax({
+            url: apiURL,
+            type: "GET",
+            crossDomain: true,
+            dataType: 'jsonp',
+            success: function(response) {
+                //The request succeeded
+                console.log(response);
+                parseWeather(response);
+                $('#loader').remove();
+            },
+            error: function(xhr, status) {
+                console.log(status);
+                $('#loader').remove();
+                $('#error').removeClass("hidden");
+                showError();
+            }
+        });
+    }
 
 
-	// Show an error if we can't access the weather
-	function showError()
-	{
-		$('#temp').text('Uh-Oh! Style Weather is currently unavailable.');
-		$('body').css('background-color','rgb(236,93,183');	
-	}
-	}
+    // Parse and use the weather values from the forecast.io JSON
 
-});
+
+    function parseWeather(data) {
+        var week = data.daily.data;
+
+        $('#temp').text("Now " + (Math.round(data.currently.apparentTemperature)));
+        $('#temp').addClass('degrees');
+        $('#sum').text(data.currently.summary);
+        $('#temp2').text(Math.round(week[1].temperatureMin));
+        $('#temp2').addClass('degrees');
+        $('#sum2').text(week[1].summary);
+        $('#temp3').text(Math.round(week[2].temperatureMin));
+        $('#temp3').addClass('degrees');
+        $('#sum3').text(week[2].summary);
+
+        console.log(week);
+        var today = data.currently.apparentTemperature;
+        var tomorrow = data.daily.data[1];
+        var dayAfterTomorrow = data.daily.data[2];
+        console.log(tomorrow.temperatureMin);
+
+        console.log(today);
+        var imageFile = parseIcon(data.currently.icon);
+        console.log("condition: " + data.currently.icon);
+        console.log("image file: " + imageFile);
+
+        $('<img>').attr("src", "images/" + imageFile).appendTo('#wrapper');
+        console.log(today.icon);
+
+        var image = parseIcon(week[1].icon);
+        $('<img>').attr("src", "images/" + image).appendTo('#wrapper');
+
+        var image = parseIcon(week[2].icon);
+        $('<img>').attr("src", "images/" + image).appendTo('#wrapper');
+    }
+
+    function parseIcon(icon) {
+        switch (icon) {
+
+            case "wind":
+            case "partly-cloudy-day":
+                var img = "partly-cloudy-day.jpg";
+                break;
+
+            case "cloudy":
+            case "partly-cloudy-night":
+                var img = "partly-cloudy-night.jpg";
+                break;
+
+            case "rain":
+                var img = "rainyDay.jpg";
+                var img = "rainyNight.jpg";
+                break;
+
+            case "snow":
+                var img = "snowyDay.jpg"
+            case "sleet":
+                var img = "snowyNight.jpg";
+
+                break;
+
+                break;
+            default:
+                "#d86b93";
+                break;
+        }
+        return img;
+    }
+
+
+    // Show an error if we can't access the weather
+    function showError() {
+        $('#temp').text('Uh-Oh! Style Weather is currently unavailable.');
+        $('body').css('background-color', 'rgb(236,93,183');
+    }
+}
+);
